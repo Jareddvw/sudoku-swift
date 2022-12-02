b692a2b08f1396ee0f04c01275e9b635794ce3c7
Jared Williams <jareddw@berkeley.edu>
2022-12-01 21:47:33 -0800

    added lots of ui for buttons and implemented solver

--- SudokuApp/ContentView.swift
+++ SudokuApp/ContentView.swift
@@ -12,12 +12,14 @@ class Square: ObservableObject {
     @Published var index: Int
     @Published var row: Int
     @Published var col: Int
+    @Published var isPermanent: Bool
     
-    init(value: Int, index: Int, rowCol:[Int]) {
+    init(value: Int, index: Int, rowCol:[Int], isPermanent:Bool) {
         self.squareVal = value
         self.index = index
         self.row = rowCol[0]
         self.col = rowCol[1]
+        self.isPermanent = isPermanent
     }
 }
 
@@ -35,15 +37,19 @@ struct SquareView: View {
         }
     }
     
+    var squareWeight: Font.Weight {
+        return self.data.isPermanent ? .bold : .light
+    }
+    
     var body: some View {
         Button(action: {
             self.onclick()
         }, label: {
             Text(self.data.squareVal != 0 ? String(self.data.squareVal) : " ")
-                .foregroundColor(Color.white)
-                .bold()
+                .foregroundColor(Color.black)
+                .fontWeight(squareWeight)
                 .frame(width:40, height:40, alignment: .center)
-                .border(Color.gray)
+                .border(Color.black.opacity(0.7))
                 .background(squareColor)
         })
     }
@@ -52,9 +58,7 @@ struct SquareView: View {
 class BoardClass: ObservableObject {
     @Published var items = [Square]()
     init() {
-        for i in 0...80 {
-            items.append(Square(value:i, index:i, rowCol:indexToRowCol(index: i)))
-        }
+        self.makeNewBoard()
     }
     
     func indexToRowCol(index: Int) -> [Int] {
@@ -62,33 +66,154 @@ class BoardClass: ObservableObject {
         let col: Int = index % 9
         return [row, col]
     }
+    
+    func rowColToIndex(row:Int, col:Int) -> Int {
+        return col + row * 9
+    }
+    
+    func setSquare(index: Int, newVal: Int) -> Void {
+        if (items[index].isPermanent) {
+            return
+        } else {
+            items[index].squareVal = newVal
+        }
+    }
+    
+    func makeNewBoard() {
+        items.removeAll()
+        for i in 0...80 {
+            let perm = Int.random(in: 0..<10)
+            items.append(Square(value:perm, index:i, rowCol:indexToRowCol(index: i), isPermanent:(arc4random() != 0 && perm != 0)))
+        }
+    }
+    
+    func isValid(rowIndex:Int, colIndex:Int, k:Int) -> Bool {
+        for i in 0 ..< 9 {
+          let m = 3 * (rowIndex / 3) + (i / 3);
+          let n = 3 * (colIndex / 3) + (i % 3);
+            if (items[rowColToIndex(row:rowIndex, col:i)].squareVal == k ||
+                items[rowColToIndex(row:i, col:colIndex)].squareVal == k ||
+                items[rowColToIndex(row:m, col:n)].squareVal == k) {
+            return false;
+          }
+      }
+      return true;
+    }
+
+    
+    func solveBoard() -> Bool {
+        for i in 0 ..< 9 {
+            for j in 0 ..< 9 {
+                if (items[rowColToIndex(row: i, col: j)].squareVal == 0) {
+                    for k in 1 ..< 10 {
+                        if (isValid(rowIndex:i, colIndex:j, k:k)) {
+                            items[rowColToIndex(row:i, col:j)].squareVal = k
+                            if (solveBoard()) {
+                                return true;
+                            } else {
+                                items[rowColToIndex(row:i, col:j)].squareVal = 0;
+                            }
+                        }
+                    }
+                return false;
+                }
+            }
+          }
+          return true;
+    }
 }
 
 
 struct ContentView: View {
     @StateObject var board = BoardClass()
-    @State var isSolved = false
+    @State var isSolved: Bool = false
+    @State var selectedVal: Int = 0
     
-    func onSquareClick(_ index: Int) {
-        return
+    func getButtonColor(currentVal: Int, selectedVal: Int) -> Color {
+        if (currentVal == selectedVal) {
+            return Color.blue.opacity(0.8)
+        } else {
+            return Color.blue.opacity(0.5)
+        }
+    }
+    
+    func setSelectedVal(selected: Int) -> Void {
+        selectedVal = selected
     }
     
     var body: some View {
         VStack(spacing:0) {
             Text("Sudoku")
                 .bold()
-                .padding(.bottom)
+                .padding([.bottom], 20)
                 .font(.title3)
-            ForEach(0 ..< board.items.count / 9, content: {
-                row in
+            ForEach(0 ..< board.items.count / 9, content: { row in
                 HStack(spacing:0) {
                     ForEach(0 ..< 9, content: {
                         col in
                         let index = row * 9 + col
-                        SquareView(data: board.items[index], onclick: {self.onSquareClick(index)})
+                        SquareView(data: board.items[index], onclick: {
+                            self.board.setSquare(index: index, newVal: selectedVal)
+                        })
                     })
                 }
             })
+            VStack(spacing:2) {
+                HStack(spacing:2) {
+                    ForEach(0 ..< 6, content: { val in
+                        Button(action: {
+                            self.setSelectedVal(selected: val)
+                        }, label: {
+                            Text(val == 0 ? "X" : String(val))
+                                .foregroundColor(Color.white)
+                                .bold()
+                                .frame(width:40, height:40, alignment: .center)
+                                .border(Color.gray)
+                                .background(getButtonColor(currentVal: val, selectedVal: selectedVal))
+                        })
+                    })
+                }
+                HStack(spacing:2) {
+                    ForEach(6 ..< 10, content: { val in
+                        Button(action: {
+                            self.setSelectedVal(selected: val)
+                        }, label: {
+                            Text(String(val))
+                                .foregroundColor(Color.white)
+                                .bold()
+                                .frame(width:40, height:40, alignment: .center)
+                                .border(Color.gray)
+                                .background(getButtonColor(currentVal: val, selectedVal: selectedVal))
+                        })
+                    })
+                }
+
+            }
+            .padding([.top, .bottom], 20)
+            HStack(spacing:2) {
+                Button(action: {
+                    self.board.makeNewBoard()
+                }, label: {
+                    Text("New Game")
+                        .foregroundColor(Color.white)
+                        .bold()
+                        .frame(alignment: .center)
+                        .padding(10)
+                        .border(Color.gray)
+                        .background(Color.gray)
+                })
+                Button(action: {
+                    self.board.solveBoard()
+                }, label: {
+                    Text("View Solution")
+                        .foregroundColor(Color.white)
+                        .bold()
+                        .frame(alignment: .center)
+                        .padding(10)
+                        .border(Color.gray)
+                        .background(Color.gray)
+                })
+            }
         }
     }
 }
--- SudokuApp/OneSquareView.swift
+++
@@ -1,8 +0,0 @@
-//
-//  OneSquareView.swift
-//  SudokuApp
-//
-//  Created by Jared Williams on 12/1/22.
-//
-
-import Foundation
