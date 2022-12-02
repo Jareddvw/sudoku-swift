8968b4cb5d1b942a7e2a021db1f4f95eaf9fd22e
Jared Williams <jareddw@berkeley.edu>
2022-12-01 18:11:53 -0800

    Initial Commit

---
+++ SudokuApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SudokuApp
+//
+//  Created by Jared Williams on 12/1/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SudokuApp/SudokuAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SudokuAppApp.swift
+//  SudokuApp
+//
+//  Created by Jared Williams on 12/1/22.
+//
+
+import SwiftUI
+
+@main
+struct SudokuAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
